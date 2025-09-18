38e2f7af72b2eb3b834485a5783cc2623f02bbc2
Shubhang- Sagar- Shukla <shubhangsagarshukla@gmail.com>
2025-09-18 18:56:09 +0530

    updated backend link

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:7000/api/v1/users", // adjust to your backend route
+  baseURL: "https://disaster-management-project-1.onrender.com/api/v1/users", // adjust to your backend route
   withCredentials: true, // 👈 important for cookies / auth
 });
