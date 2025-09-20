7ebfc3f3d72893952374c7bb9f247278f454b290
Shubhang- Sagar- Shukla <shubhangsagarshukla@gmail.com>
2025-09-21 02:08:41 +0530

    modified middleware from throw error to returning error with next command

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -29,7 +29,7 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
         next();
 
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token");
+         return next(new ApiError(401, error?.message || "Invalid access token"));
     }
 });
 
